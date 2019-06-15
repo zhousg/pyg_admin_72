03432f66256a33995ee81bced8bd0e43b9507114
zhousg <zhoushugang@itcast.cn>
2019-06-15 17:46:01 +0800

    商品列表完成-表单准备和校验

--- src/views/goods/GoodsAdd-Mixin.js
+++ src/views/goods/GoodsAdd-Mixin.js
@@ -1,7 +1,71 @@
 export default {
   data() {
-    return {
-      msg: "商品添加"
+    const checkCat = (rule, value, callback) => {
+      if (this.cascaderValues.length !== 3) {
+        return callback(new Error("商品的分类必须选中三级分类"));
+      }
+      callback();
     };
+    return {
+      // 当前完成的步骤
+      stepActive: 0,
+      // 表单数据
+      form: {
+        goods_name: "",
+        // 三级分类以逗号分隔 1,10,100
+        goods_cat: "",
+        goods_price: "",
+        goods_number: "",
+        goods_weight: ""
+      },
+      // 定义校验规则
+      rules: {
+        goods_name: [
+          { required: true, message: "商品名称必填", trigger: "blur" }
+        ],
+        goods_cat: [
+          {
+            validator: checkCat,
+            trigger: "change"
+          }
+        ],
+        goods_price: [
+          { required: true, message: "商品价格必填", trigger: "blur" }
+        ],
+        goods_number: [
+          { required: true, message: "商品数量必填", trigger: "blur" }
+        ],
+        goods_weight: [
+          { required: true, message: "商品重量必填", trigger: "blur" }
+        ]
+      },
+      // 级联数据
+      cascaderValues: [],
+      cascaderOptions: []
+    };
+  },
+  created() {
+    // 获取组件初始化需要的数据  其中级联的数据
+    this.getData();
+  },
+  methods: {
+    // tab切换事件  点击tab的事件
+    changeTab(tab) {
+      // tab 是当前选中的tab的实例对象   包含信息
+      // tab.index 当前tab的标识  索引类型是字符串
+      // "0" "1" "2" "3" "4" 对应五个tab
+      // +"100" 隐式转换
+      this.stepActive = +tab.index;
+    },
+    // 级联选择事件
+    cascaderChange() {},
+    // 获取级联数据
+    async getData() {
+      const {
+        data: { data, meta }
+      } = await this.$http.get("categories");
+      if (meta.status !== 200) return this.$message.error("获取级联信息失败");
+      this.cascaderOptions = data;
+    }
   }
 };
--- src/views/goods/GoodsAdd.vue
+++ src/views/goods/GoodsAdd.vue
@@ -5,7 +5,7 @@
       <el-alert title="请按照步骤录入商品信息" type="info" center show-icon>
       </el-alert>
       <!-- :active  当前激活的步骤（完成的步骤）-->
-      <el-steps :active="1" align-center>
+      <el-steps :active="stepActive" align-center>
         <el-step title="基本信息"></el-step>
         <el-step title="商品参数"></el-step>
         <el-step title="商品属性"></el-step>
@@ -13,8 +13,43 @@
         <el-step title="商品内容"></el-step>
       </el-steps>
       <!-- tabs -->
-      <el-tabs tab-position="left" style="height: 200px;">
-        <el-tab-pane label="基本信息">基本信息</el-tab-pane>
+      <el-tabs @tab-click="changeTab" tab-position="left">
+        <el-tab-pane label="基本信息">
+          <el-form
+            style="margin-left:50px"
+            ref="form"
+            :model="form"
+            :rules="rules"
+            label-width="80px"
+            autocomplete="off"
+          >
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input style="width:600px"></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类" prop="goods_cat">
+              <el-cascader
+                style="width:300px"
+                v-model="cascaderValues"
+                :options="cascaderOptions"
+                :props="{
+                  expandTrigger: 'hover',
+                  label: 'cat_name',
+                  value: 'cat_id'
+                }"
+                @change="cascaderChange"
+              ></el-cascader>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input style="width:300px"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input style="width:300px"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input style="width:300px"></el-input>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
         <el-tab-pane label="商品参数">商品参数</el-tab-pane>
         <el-tab-pane label="商品属性">商品属性</el-tab-pane>
         <el-tab-pane label="商品图片">商品图片</el-tab-pane>
